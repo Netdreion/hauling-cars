454f813305af39ef25825a1b4e19f855615a60d7
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-07-12 01:01:16 -0400

    offer page set-up

--- src/Data.jsx
+++ src/Data.jsx
@@ -15,9 +15,139 @@ const data = [
     offer: 1500,
   },
   {
-    id: "CALIFORNIA",
+    id: "california",
     distance: 1300,
     offer: 1300,
+  },
+  {
+    id: "new york",
+    distance: 200,
+    offer: 800,
+  },
+  {
+    id: "florida",
+    distance: 900,
+    offer: 1000,
+  },
+  {
+    id: "texas",
+    distance: 600,
+    offer: 1200,
+  },
+  {
+    id: "arizona",
+    distance: 1100,
+    offer: 900,
+  },
+  {
+    id: "georgia",
+    distance: 750,
+    offer: 700,
+  },
+  {
+    id: "illinois",
+    distance: 800,
+    offer: 1500,
+  },
+  {
+    id: "colorado",
+    distance: 1000,
+    offer: 1100,
+  },
+  {
+    id: "washington",
+    distance: 1300,
+    offer: 1600,
+  },
+  {
+    id: "oregon",
+    distance: 1200,
+    offer: 900,
+  },
+  {
+    id: "michigan",
+    distance: 700,
+    offer: 800,
+  },
+  {
+    id: "ohio",
+    distance: 600,
+    offer: 1200,
+  },
+  {
+    id: "pennsylvania",
+    distance: 500,
+    offer: 1100,
+  },
+  {
+    id: "north carolina",
+    distance: 800,
+    offer: 1000,
+  },
+  {
+    id: "tennessee",
+    distance: 900,
+    offer: 1300,
+  },
+  {
+    id: "louisiana",
+    distance: 1000,
+    offer: 700,
+  },
+  {
+    id: "oklahoma",
+    distance: 1100,
+    offer: 900,
+  },
+  {
+    id: "kentucky",
+    distance: 600,
+    offer: 800,
+  },
+  {
+    id: "alabama",
+    distance: 400,
+    offer: 1100,
+  },
+  {
+    id: "mississippi",
+    distance: 500,
+    offer: 1000,
+  },
+  {
+    id: "missouri",
+    distance: 800,
+    offer: 1200,
+  },
+  {
+    id: "kansas",
+    distance: 900,
+    offer: 1300,
+  },
+  {
+    id: "nevada",
+    distance: 1200,
+    offer: 1500,
+  },
+  {
+    id: "utah",
+    distance: 1100,
+    offer: 1400,
+  },
+  {
+    id: "new mexico",
+    distance: 1000,
+    offer: 800,
+  },
+  {
+    id: "minnesota",
+    distance: 700,
+    offer: 1000,
+  },
+  {
+    id: "wisconsin",
+    distance: 600,
+    offer: 900,
   },
 ];
 
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import data from "../Data";
-
+import "../App.css";
 const HomePage = () => {
   const [locations, setLocations] = useState(data);
 
@@ -10,16 +10,29 @@ const HomePage = () => {
 
   return (
     <div>
-      <form>
-        <div>
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-        </div>
-        <button>calculate</button>
-      </form>
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Distance</th>
+              <th>Offer</th>
+              <th>Ratio</th>
+            </tr>
+          </thead>
+          <tbody>
+            {locations.map((location) => (
+              <tr key={location.id}>
+                <button>
+                  <td>{location.distance} miles</td>
+                  <td>{location.offer}$</td>
+                </button>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <input type="text" />
+      </div>
     </div>
   );
 };
